870eb951cf9153d80fb73c02ded66630cabbd5a4
jvwilliams23 <josh.williams@stfc.ac.uk>
2025-01-17 13:35:03 +0000

    add linesampler to team13 example

--- examples/team13/Main.cpp
+++ examples/team13/Main.cpp
@@ -106,6 +106,10 @@ main(int argc, char * argv[])
   // Set Mesh
   mfem::Mesh mesh((std::string(DATA_DIR) + std::string("./vac_team13.e")).c_str(), 1, 1);
   auto pmesh = std::make_shared<mfem::ParMesh>(MPI_COMM_WORLD, mesh);
+  mfem::H1_FECollection fecm(1, 3);
+  mfem::ParFiniteElementSpace pfespace(pmesh.get(), &fecm, 3);
+  // Necessary, in case the nodal FE space is not set on the pmesh because it is lowest order.
+  pmesh->SetNodalFESpace(&pfespace);
 
   int par_ref_lvl = 0;
   for (int l = 0; l < par_ref_lvl; ++l)
@@ -128,25 +132,82 @@ main(int argc, char * argv[])
 
   hephaestus::Outputs outputs = defineOutputs();
   problem_builder->SetOutputs(outputs);
+  {
+    // Call LineSampler to save values
+    std::string gridfunction_name("magnetic_flux_density");
+    const int num_pts = 100;
+    // Mesh bounding box (for the full serial mesh).
+    mfem::Vector pos_min(3), pos_max(3);
+    pos_min(0) = 1.6 / 2.0 / 1000.0; // 0<x<1.6 (mm) -> halfway point
+    pos_max(0) = pos_min(0);
+    pos_min(1) = 0.0;
+    pos_max(1) = 0.0;
+    pos_min(2) = 0.0;
+    pos_max(2) = 60.0 / 1000.0;
+    std::string csv_name_ab("fluxDensityProfileAB.csv");
+    std::shared_ptr<hephaestus::LineSamplerAux> linesamplerwriter_ab =
+        std::make_shared<hephaestus::LineSamplerAux>(gridfunction_name,
+                                                     pos_min,
+                                                     pos_max,
+                                                     num_pts,
+                                                     csv_name_ab,
+                                                     "t_s, x_m, y_m, z_m, B_x_T, B_y_T, B_z_T");
+    linesamplerwriter_ab->SetPriority(5);
+    problem_builder->AddPostprocessor("LineSamplerWriterAB", linesamplerwriter_ab);
 
-  hephaestus::InputParameters solver_options;
-  solver_options.SetParam("Tolerance", float(1.0e-13));
-  solver_options.SetParam("AbsTolerance", float(1.0e-16));
-  solver_options.SetParam("MaxIter", (unsigned int)500);
-  problem_builder->SetSolverOptions(solver_options);
+    pos_min(0) = 2.1 / 1000.0; // 0<x<1.6 (mm) -> halfway point
+    pos_max(0) = 122.1 / 1000.0;
+    pos_min(1) = (65.0 + 15.0) / 2.0 / 1000.0;
+    pos_max(1) = (65.0 + 15.0) / 2.0 / 1000.0;
+    pos_min(2) = (63.2 + 60.0) / 2.0 / 1000.0;
+    pos_max(2) = (63.2 + 60.0) / 2.0 / 1000.0;
+    std::string csv_name_cd("fluxDensityProfileCD.csv");
+    std::shared_ptr<hephaestus::LineSamplerAux> linesamplerwriter_cd =
+        std::make_shared<hephaestus::LineSamplerAux>(gridfunction_name,
+                                                     pos_min,
+                                                     pos_max,
+                                                     num_pts,
+                                                     csv_name_cd,
+                                                     "t_s, x_m, y_m, z_m, B_x_T, B_y_T, B_z_T");
+    linesamplerwriter_cd->SetPriority(5);
+    problem_builder->AddPostprocessor("LineSamplerWriterCD", linesamplerwriter_cd);
 
-  problem_builder->FinalizeProblem();
+    pos_min(0) = (125.3 + 122.1) / 2.0 / 1000.0;
+    pos_max(0) = (125.3 + 122.1) / 2.0 / 1000.0;
+    pos_min(1) = (65.0 + 15.0) / 2.0 / 1000.0;
+    pos_max(1) = (65.0 + 15.0) / 2.0 / 1000.0;
+    pos_min(2) = 0.0;
+    pos_max(2) = (60.0) / 1000.0;
+    std::string csv_name_ef("fluxDensityProfileEF.csv");
+    std::shared_ptr<hephaestus::LineSamplerAux> linesamplerwriter_ef =
+        std::make_shared<hephaestus::LineSamplerAux>(gridfunction_name,
+                                                     pos_min,
+                                                     pos_max,
+                                                     num_pts,
+                                                     csv_name_ef,
+                                                     "t_s, x_m, y_m, z_m, B_x_T, B_y_T, B_z_T");
+    linesamplerwriter_ef->SetPriority(5);
+    problem_builder->AddPostprocessor("LineSamplerWriterEF", linesamplerwriter_ef);
 
-  auto problem = problem_builder->ReturnProblem();
-  hephaestus::InputParameters exec_params;
-  exec_params.SetParam("VisualisationSteps", int(1));
-  exec_params.SetParam("UseGLVis", true);
-  exec_params.SetParam("Problem", static_cast<hephaestus::SteadyStateProblem *>(problem.get()));
+    hephaestus::InputParameters solver_options;
+    solver_options.SetParam("Tolerance", float(1.0e-13));
+    solver_options.SetParam("AbsTolerance", float(1.0e-16));
+    solver_options.SetParam("MaxIter", (unsigned int)500);
+    problem_builder->SetSolverOptions(solver_options);
 
-  auto executioner = std::make_unique<hephaestus::SteadyExecutioner>(exec_params);
+    problem_builder->FinalizeProblem();
 
-  hephaestus::logger.info("Created executioner");
-  executioner->Execute();
+    auto problem = problem_builder->ReturnProblem();
+    hephaestus::InputParameters exec_params;
+    exec_params.SetParam("VisualisationSteps", int(1));
+    exec_params.SetParam("UseGLVis", true);
+    exec_params.SetParam("Problem", static_cast<hephaestus::SteadyStateProblem *>(problem.get()));
+
+    auto executioner = std::make_unique<hephaestus::SteadyExecutioner>(exec_params);
+
+    hephaestus::logger.info("Created executioner");
+    executioner->Execute();
+  }
 
   MPI_Finalize();
 }
